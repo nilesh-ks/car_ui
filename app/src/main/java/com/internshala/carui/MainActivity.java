25e5d2701cef47465b06b91103aaf1bdf572fa65
Project <nileshsinha1731@gmail.com>
2021-04-06 01:09:48 +0530

    Initial commit

---
+++ app/src/main/java/com/internshala/carui/MainActivity.java
@@ -0,0 +1,18 @@
+package com.internshala.carui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       /* Window g = getWindow();
+        g.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);*/
+    }
+}
